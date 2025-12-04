bf5c780249c178ff83ced5982b6d62b4eddcea72
Mehrdad Mohammadi <33103898+mehrdadmhmdi@users.noreply.github.com>
2025-12-04 00:55:35 -0600

    Update optimize.py
    LinAlgError removed

--- src/ke_drl/optimize.py
+++ src/ke_drl/optimize.py
@@ -5,7 +5,6 @@ from torch import optim
 from torch.nn.utils import clip_grad_norm_
 from scipy.sparse.linalg import eigsh, LinearOperator
 from scipy.linalg import cho_factor, cho_solve, eigh
-from scipy.linalg import LinAlgError
 
 
 class RKDRL_Optimizer:
@@ -355,6 +354,7 @@ class RKDRL_Optimizer:
         history_obj = [math.log(x) for x in history_obj]
 
         return B.detach().cpu(), history_obj, history_be
+
 
 
 
